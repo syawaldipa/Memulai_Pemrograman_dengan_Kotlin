0995dcfc3bc9cd149cb367f58c5e31b4940aa7f6
unknown <ardisyawaldipa@gmail.com>
2019-09-28 13:37:43 +0700

    Initial commit

---
+++ Membangun dan Menjalankan Program Kotlin/Latihan/Latihan 1/src/Task.kt
@@ -0,0 +1,14 @@
+
+ //cara ke 1
+fun main() {
+    println("Kotlin is Awesome!")
+}
+//------------------------------------------
+                //cara ke 2, hasil  sama tapi incorrect
+//fun main() {
+//    val line = """ Kotlin is Awesome!
+//    """.trimIndent()
+//
+//
+//    println(line)
+//}
---
+++ Membangun dan Menjalankan Program Kotlin/Latihan/Latihan 2/src/Task.kt
@@ -0,0 +1,18 @@
+//cara pertama
+//fun main() {
+//    val line = """
+//        Kotlin,
+//        is Awesome!
+//    """.trimIndent()
+//
+//
+//    println(line)
+//}
+
+//-----------------------------
+//cara ke 2
+fun main() {
+    println("Kotlin,")
+    println("is Awesome!")
+}
+
